15c7a88f5980eee57270d87990d1cb3a9fdc9d5d
SpeedyZapGaming <SpeedyZapGaming@gmail.com>
2017-04-10 20:19:34 +0200

    Update LolTranslator.py

--- Game Code/LolTranslator.py
+++ Game Code/LolTranslator.py
@@ -1,8 +1,18 @@
+""" This is the lol-translator by: SpeedyZapGaming """
+
+# Variable
 lollish = "ololly"
 
-print ("Hello and welcome to the lol translator!")
-print ("REMEMBER: No numbers!")
+# Intro
+print ("Hello and welcome to the lol-translator!")
+print ("This translator was made for fun by SpeedyZapGaming.")
+print ("If you are gonna re-upload this then please give credits to SpeedyZapGaming.")
+print ("This translator does NOT accept numbers.")
+
+# Asks for word
 Word = input("What is the word you want us to translate?: ")
+
+# Result
 if len (Word) > 0 and Word.isalpha():
     TheWord = Word.lower()
     FirstLetter = TheWord[0]
@@ -15,7 +25,10 @@ if len (Word) > 0 and Word.isalpha():
         print("The result is,", NewWord)
         print ("Thank you for using the lol translator. See you soon!")
 else:
-    print ("You have typed in nothing or it contained numbers.")
-    print ("Thank you for using the lol translator. See you soon!")
+    print ("You have typed in nothing.")
+    print ("OR... You typed in numbers.")
+    print ("Please restart the translator.")
+
+""" Thank you for using the lol-translator by: SpeedyZapGaming """
 
 
